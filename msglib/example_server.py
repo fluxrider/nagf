b772a58a9a58503e757d5165d7846fa94cb87c79
fluxrider <doompilot@gmail.com>
2020-05-09 20:36:01 -0400

    (tested) python msgmgr utils

--- msglib/example_server.py
+++ msglib/example_server.py
@@ -1,37 +1,12 @@
 #!/usr/bin/env python3
 # Copyright 2020 David Lareau. This program is free software under the terms of the GPL-3.0-or-later, no warranty.
-# LD_LIBRARY_PATH=. ./example_server.py
+# LD_LIBRARY_PATH=. python -B example_server.py
 
-import time
-import os
-import ctypes
-msglib = ctypes.CDLL("msglib.so", use_errno=True)
-msglib.msglib_connect.restype = ctypes.c_void_p
-msglib.msglib_get_mem.restype = ctypes.POINTER(ctypes.c_byte)
-msglib.msglib_wait.restype = ctypes.c_char_p
-msglib.msglib_post.restype = ctypes.c_char_p
-msglib.msglib_disconnect.restype = ctypes.c_char_p
+from msgmgr import MsgMgr
 
-# connect to message manager
-length = 1024
-name = "/example-msgmgr"
-error = ctypes.c_int()
-line = ctypes.c_int()
-msgmgr = ctypes.c_void_p(msglib.msglib_connect(name.encode(), length, True, ctypes.pointer(error), ctypes.pointer(line)))
-if error.value != 0: raise RuntimeError(f'msglib_connect:{line.value}: {os.strerror(error.value)}')
-
-msg = msglib.msglib_get_mem(msgmgr)
-
-error = msglib.msglib_wait(msgmgr, ctypes.c_double(4), ctypes.pointer(line))
-if error: raise RuntimeError(f'msglib_wait:{line.value}: {error.decode()}')
-
-print(msg[0])
-msg[0] = 42
-
-error = msglib.msglib_post(msgmgr, ctypes.pointer(line))
-if error: raise RuntimeError(f'msglib_post:{line.value}: {error.decode()}')
-
-time.sleep(2)
-
-error = msglib.msglib_disconnect(msgmgr, ctypes.pointer(line))
-if error: raise RuntimeError(f'msglib_disconnect:{line.value}: {error.decode()}')
+with MsgMgr('/example-msgmgr', is_server=True) as server:
+  data = server.receive()
+  print(len(data))
+  print(data)
+  print(bytearray(data).decode())
+  server.reply("whatever".encode())
--- msglib/msgmgr.py
+++ msglib/msgmgr.py
@@ -2,7 +2,7 @@
 # Copyright 2020 David Lareau. This program is free software under the terms of the GPL-3.0-or-later, no warranty.
 
 import os
-import mmap
+import sys
 import ctypes
 msglib = ctypes.CDLL("msglib.so", use_errno=True)
 msglib.msglib_connect.restype = ctypes.c_void_p
@@ -24,14 +24,15 @@ class MsgMgr:
     line = ctypes.c_int()
     self.msgmgr = ctypes.c_void_p(msglib.msglib_connect(name.encode(), length, is_server, ctypes.pointer(error), ctypes.pointer(line)))
     if error.value != 0: raise RuntimeError(f'msglib_connect:{line.value}: {os.strerror(error.value)}')
-    self.msg = msglib.msglib_get_mem(msgmgr)
+    self.msg = msglib.msglib_get_mem(self.msgmgr)
 
   def disconnect(self):
     line = ctypes.c_int()
     error = msglib.msglib_disconnect(self.msgmgr, ctypes.pointer(line))
     if error: raise RuntimeError(f'msglib_disconnect:{line.value}: {error.decode()}')
 
-  def client_send(self, data):
+  # client interface (i.e. send a message and wait for a reply from the server)
+  def send(self, data):
     if self.is_server: raise RuntimeError()
     line = ctypes.c_int()
     # lock
@@ -55,25 +56,26 @@ class MsgMgr:
     if error: raise RuntimeError(f'msglib_wait:{line.value}: {error.decode()}')
     # read header
     length = int.from_bytes(self.msg[:4], byteorder=sys.byteorder, signed=False)
-    # copy data (because client must not retain pointer to shm) TODO unless they are alone
-    data = [self.msg[i] for i in range(4, length + 4)] 
+    # copy data (see slicing comment in receive() TODO avoid the copy if no multi-client support)
+    data = self.msg[4:length+4]
     # unlock
     if self.use_multi_client_lock:
       error = msglib.msglib_unlock(self.msgmgr, ctypes.pointer(line))
       if error: raise RuntimeError(f'msglib_unlock:{line.value}: {error.decode()}')
     return data
 
-  def server_receive(self):
+  # server interface (i.e. receive a message from the client, process, then reply)
+  def receive(self):
     if not self.is_server: raise RuntimeError()
     line = ctypes.c_int()
     error = msglib.msglib_wait(self.msgmgr, ctypes.c_double(self.timeout), ctypes.pointer(line))
     if error: raise RuntimeError(f'msglib_wait:{line.value}: {error.decode()}')
     # read header
     length = int.from_bytes(self.msg[:4], byteorder=sys.byteorder, signed=False)
-    # return data (servers own the shm until they reply so it's ok not to copy it into a python array)
+    # return data (in python, slicing a ctypes array seems to do a copy TODO can it be avoided?)
     return self.msg[4:length+4]
 
-  def server_reply(self, data):
+  def reply(self, data):
     if not self.is_server: raise RuntimeError()
     # header: len(data)
     header = len(data).to_bytes(4, byteorder=sys.byteorder, signed=False)

650404da8b79d37636310b79f6213fee530e5f72
fluxrider <doompilot@gmail.com>
2020-05-27 15:50:26 -0400

    benchmark python server

--- libsrr/benchmark.sh
+++ libsrr/benchmark.sh
@@ -3,31 +3,19 @@ set -e
 gcc -o benchmark_server benchmark_server.c -L. -lsrr
 gcc -o benchmark_client benchmark_client.c -L. -lsrr
 
+# C server
 echo 'Launch C server'
 LD_LIBRARY_PATH=. ./benchmark_server &
 server_pid=$!
 sleep .4
-
 echo 'Launch C client (ignore this one)'
-LD_LIBRARY_PATH=. ./benchmark_client &
-client_pid=$!
-wait $client_pid
-
+LD_LIBRARY_PATH=. ./benchmark_client
 echo 'Launch C client'
-LD_LIBRARY_PATH=. ./benchmark_client &
-client_pid=$!
-wait $client_pid
-
+LD_LIBRARY_PATH=. ./benchmark_client
 echo 'Launch python client'
 LD_LIBRARY_PATH=. python -B benchmark_client.py
-client_pid=$!
-wait $client_pid
-
 echo 'Launch C client with mutex'
-LD_LIBRARY_PATH=. ./benchmark_client multi &
-client_pid=$!
-wait $client_pid
-
+LD_LIBRARY_PATH=. ./benchmark_client multi
 echo 'Launch 3 C clients with mutex'
 LD_LIBRARY_PATH=. ./benchmark_client multi &
 client_pid=$!
@@ -36,7 +24,39 @@ client_pid_2=$!
 LD_LIBRARY_PATH=. ./benchmark_client multi &
 client_pid_3=$!
 wait $client_pid $client_pid_2 $client_pid_3
-
+set +e
 wait $server_pid
+set -e
+
+# python server
+echo 'Launch python server'
+LD_LIBRARY_PATH=. python -B benchmark_server.py &
+server_pid=$!
+sleep .4
+echo 'Launch C client (ignore this one)'
+LD_LIBRARY_PATH=. ./benchmark_client
+echo 'Launch C client'
+LD_LIBRARY_PATH=. ./benchmark_client
+echo 'Launch python client'
+LD_LIBRARY_PATH=. python -B benchmark_client.py
+echo 'Launch C client with mutex'
+LD_LIBRARY_PATH=. ./benchmark_client multi
+echo 'Launch 3 C clients with mutex'
+LD_LIBRARY_PATH=. ./benchmark_client multi &
+client_pid=$!
+LD_LIBRARY_PATH=. ./benchmark_client multi &
+client_pid_2=$!
+LD_LIBRARY_PATH=. ./benchmark_client multi &
+client_pid_3=$!
+wait $client_pid $client_pid_2 $client_pid_3
+set +e
+wait $server_pid
+set -e
+
+# cleanup
 rm benchmark_client
 rm benchmark_server
+
+# computer details
+uname -a
+cat /proc/cpuinfo
---
+++ libsrr/benchmark_server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright 2020 David Lareau. This program is free software under the terms of the GPL-3.0-or-later, no warranty.
+# LD_LIBRARY_PATH=. python -B benchmark_server.py
+
+from srr import Srr
+import time
+import sys
+
+with Srr('/benchmark-srr', is_server=True, timeout=2) as server:
+  while True:
+    data = server.receive()
+    x = int.from_bytes(data, byteorder=sys.byteorder, signed=False)
+    x += 5
+    server.reply(x.to_bytes(4, byteorder=sys.byteorder, signed=False))

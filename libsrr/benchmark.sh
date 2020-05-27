bd994a3196408471f9a8935d2cf05824efd5387b
fluxrider <doompilot@gmail.com>
2020-05-27 15:37:32 -0400

    multi client benchmark

--- libsrr/benchmark.sh
+++ libsrr/benchmark.sh
@@ -8,6 +8,11 @@ LD_LIBRARY_PATH=. ./benchmark_server &
 server_pid=$!
 sleep .4
 
+echo 'Launch C client (ignore this one)'
+LD_LIBRARY_PATH=. ./benchmark_client &
+client_pid=$!
+wait $client_pid
+
 echo 'Launch C client'
 LD_LIBRARY_PATH=. ./benchmark_client &
 client_pid=$!
@@ -17,6 +22,20 @@ echo 'Launch python client'
 LD_LIBRARY_PATH=. python -B benchmark_client.py
 client_pid=$!
 wait $client_pid
+
+echo 'Launch C client with mutex'
+LD_LIBRARY_PATH=. ./benchmark_client multi &
+client_pid=$!
+wait $client_pid
+
+echo 'Launch 3 C clients with mutex'
+LD_LIBRARY_PATH=. ./benchmark_client multi &
+client_pid=$!
+LD_LIBRARY_PATH=. ./benchmark_client multi &
+client_pid_2=$!
+LD_LIBRARY_PATH=. ./benchmark_client multi &
+client_pid_3=$!
+wait $client_pid $client_pid_2 $client_pid_3
 
 wait $server_pid
 rm benchmark_client
--- libsrr/benchmark_client.c
+++ libsrr/benchmark_client.c
@@ -1,9 +1,10 @@
 // Copyright 2020 David Lareau. This program is free software under the terms of the GPL-3.0-or-later, no warranty.
 // gcc -o benchmark_client benchmark_client.c -L. -lsrr
-// LD_LIBRARY_PATH=. ./benchmark_server
+// LD_LIBRARY_PATH=. ./benchmark_client
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <time.h>
 #include <inttypes.h>
 #include "srr.h"
@@ -14,14 +15,15 @@ uint64_t currentTimeMillis() {
   return tp.tv_sec * 1000 + tp.tv_nsec / 1000000;
 }
 
-void main(void) {
+void main(int argc, char * argv[]) {
   srand(time(NULL));
+  bool multi = argc == 2;
   
   // connect
   const char * error;
   struct srr client;
-  error = srr_init(&client, "/benchmark-srr", 8192, false, false, 2); if(error) { printf("srr_init: %s\n", error); exit(EXIT_FAILURE); }
-  struct srr_direct * mem = srr_direct(&client);
+  error = srr_init(&client, "/benchmark-srr", 8192, false, multi, 2); if(error) { printf("srr_init: %s\n", error); exit(EXIT_FAILURE); }
+  struct srr_direct * mem = multi? malloc(sizeof(struct srr_direct) + sizeof(uint32_t)) : srr_direct(&client);
 
   // send random ints for 2 seconds
   uint64_t t0 = currentTimeMillis();
@@ -29,13 +31,12 @@ void main(void) {
   do {
     uint32_t x = rand();
     *(uint32_t *)mem->msg = x;
-    error = srr_send(&client, sizeof(uint32_t)); if(error) { printf("srr_send: %s\n", error); exit(EXIT_FAILURE); }
+    error = srr_send_dx(&client, mem->msg, sizeof(uint32_t), mem->msg, &mem->length);
+    if(error) { printf("srr_send_dx: %s\n", error); exit(EXIT_FAILURE); }
     if(mem->length != sizeof(uint32_t)) { printf("unexpected message size: %u\n", mem->length); exit(EXIT_FAILURE); }
-    else {
-      // verify we got (x+5)
-      if(*(uint32_t*)mem->msg != x + 5) { printf("bad answer: %u != %u\n", *(uint32_t*)mem->msg, x + 5); exit(EXIT_FAILURE); }
-      count++;
-    }
+    // verify we got (x+5)
+    if(*(uint32_t*)mem->msg != x + 5) { printf("bad answer: %u != %u\n", *(uint32_t*)mem->msg, x + 5); exit(EXIT_FAILURE); }
+    count++;
   } while(currentTimeMillis() < t0 + 5000);
   printf("%" PRIu64 " send/receive/reply in 5 seconds (%f per second)\n", count, count / 5.0);
   

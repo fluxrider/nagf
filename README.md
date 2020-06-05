7bf764f925c02030b263c776317cded5daeb06a1
fluxrider <doompilot@gmail.com>
2020-06-05 16:38:41 -0400

    tileset and first tilemap

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Under Construction
 Not Another Gamedev Framework
 
-Demo's images, music, sounds and maps (*.png, *.ogg, *.tmx) are licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0">CC BY-NC-SA 4.0 <img src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a>.
+Demo's images, music, sounds and maps (*.png, *.ogg, *.tmx) are authored by myself, David Lareau, and licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0">CC BY-NC-SA 4.0 <img src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a>,
+unless a different license is specified in its filename.
+For example, the author of overworld.CC0.ArMM1998.png is ArMM1998 and the image is dedicated to the public domain (<a href="https://creativecommons.org/publicdomain/zero/1.0/">CC0</a>).
---
+++ demos/zeldaish/overworld.tsx
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="overworld.CC0.ArMM1998.png" width="640" height="576"/>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="200"/>
+   <frame tileid="41" duration="200"/>
+   <frame tileid="42" duration="200"/>
+   <frame tileid="43" duration="200"/>
+   <frame tileid="80" duration="200"/>
+   <frame tileid="81" duration="200"/>
+   <frame tileid="82" duration="200"/>
+   <frame tileid="83" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="207" type="block"/>
+ <tile id="208" type="block"/>
+ <tile id="209" type="block"/>
+ <tile id="365" probability="0.2"/>
+ <tile id="367" probability="0.2"/>
+ <tile id="368" probability="0.2"/>
+ <tile id="382" type="block">
+  <animation>
+   <frame tileid="382" duration="140"/>
+   <frame tileid="385" duration="140"/>
+   <frame tileid="388" duration="140"/>
+  </animation>
+ </tile>
+ <tile id="383" type="block">
+  <animation>
+   <frame tileid="383" duration="140"/>
+   <frame tileid="386" duration="140"/>
+   <frame tileid="389" duration="140"/>
+  </animation>
+ </tile>
+ <tile id="384" type="block">
+  <animation>
+   <frame tileid="384" duration="140"/>
+   <frame tileid="387" duration="140"/>
+   <frame tileid="390" duration="140"/>
+  </animation>
+ </tile>
+ <tile id="405" probability="0.2"/>
+ <tile id="407" probability="0.1"/>
+ <tile id="408" probability="0.3"/>
+ <tile id="422" type="block">
+  <animation>
+   <frame tileid="422" duration="140"/>
+   <frame tileid="425" duration="140"/>
+   <frame tileid="428" duration="140"/>
+  </animation>
+ </tile>
+ <tile id="423" type="block">
+  <animation>
+   <frame tileid="423" duration="140"/>
+   <frame tileid="426" duration="140"/>
+   <frame tileid="429" duration="140"/>
+  </animation>
+ </tile>
+ <tile id="424" type="block">
+  <animation>
+   <frame tileid="424" duration="140"/>
+   <frame tileid="427" duration="140"/>
+   <frame tileid="430" duration="140"/>
+  </animation>
+ </tile>
+ <tile id="462" type="block"/>
+ <tile id="463" type="block"/>
+ <tile id="464" type="block"/>
+ <tile id="521" type="block"/>
+ <tile id="522" type="block"/>
+ <tile id="523" type="block"/>
+ <tile id="560" type="block"/>
+ <tile id="561" type="block"/>
+ <tile id="562" type="block"/>
+ <tile id="563" type="block"/>
+ <tile id="600" type="block"/>
+ <tile id="601" type="block"/>
+ <tile id="602" type="block"/>
+ <tile id="603" type="block"/>
+ <tile id="640" type="block"/>
+ <tile id="641" type="block"/>
+ <tile id="680" type="block"/>
+ <tile id="682" type="block"/>
+ <tile id="683" type="block"/>
+ <tile id="684" type="block"/>
+ <tile id="720" type="block"/>
+ <tile id="722" type="block"/>
+ <tile id="723" type="block"/>
+ <tile id="724" type="block"/>
+ <tile id="760" type="block"/>
+ <tile id="761" type="block"/>
+</tileset>
